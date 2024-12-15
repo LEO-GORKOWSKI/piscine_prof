6774eaba80a34ea799c18b6957085d6fc478d04b
LEO-GORKOWSKI <leo.gorkowski@outlook.fr>
2024-12-15 14:34:04 +0100

    Question 3 : Ajout des dates dans le programme

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def cmd_ajout(liste):
     a = input("Qui nage ? ")
     b = input("quelle nage ? ")
     c = input("combien de longueur ? ")
-    liste.append((a,b,c))
+    d = input("Quelle est la date (YYYY-MM-DD) ? ")
+    liste.append((a, b, c, d))
 
 def get_cmd():
     '''Menu d'affichage avec un numéro qui donne une  commande'''
@@ -30,30 +31,30 @@ def get_cmd():
 
 def cmd_liste(liste):
     """Affiche toutes les performances des nageurs"""
-    print("Prénom      |  nage   |  longueur")
-    print("---------------------------------")
+    print("Prénom      |  nage   |  longueur |  date")
+    print("--------------------------------------------------")
     for elt in liste:
-        print(f" {elt[0]:11}| {elt[1]:8}|  {elt[2]}")
+        print(f" {elt[0]:11}| {elt[1]:8}|  {elt[2]:8} | {elt[3]}")
 
 def cmd_nageur(liste):
     """Affiche toutes les performances d'un nageur"""
     tmp = input("Quel nageur ? ")
     print("Performances de ", tmp)
-    print("  nage   |  longueur")
+    print("  nage   |  longueur |  date")
     print("--------------------")
     for elt in liste:
         if elt[0]== tmp:
-            print(f" {elt[1]:8}|  {elt[2]}")
+            print(f" {elt[0]:11}| {elt[1]:8}|  {elt[2]:8} | {elt[3]}")
 
 def cmd_nage(liste):
     """Affiche toutes les performances suivant une nage donnée"""
     tmp = input("Quel nage ? ")
     print("Nage ", tmp)
-    print(" Nageur     |  longueur")
+    print(" Nageur     |  longueur |  date")
     print("------------------------")
     for elt in liste:
         if elt[1]== tmp:
-            print(f" {elt[0]:11}|  {elt[2]}")
+            print(f" {elt[0]:11}|  {elt[2]:8}")
 
 def cmd_exit(liste):
     tmp = input("En êtes-vous sûr ? (o)ui/(n)on ")
